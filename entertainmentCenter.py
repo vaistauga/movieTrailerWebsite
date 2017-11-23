fbc9475e803acaf1afa4fbe74ead4a71ed75d3f4
Vaidotas Staugaitis <vstaugaitis@sanityassembly.com>
2017-11-22 21:08:37 -0600

    Implemented Media Inheratance Tree

--- entertainmentCenter.py
+++ entertainmentCenter.py
@@ -1,35 +1,47 @@
 import media
 import fresh_tomatoes
 
-inception = media.Movie("Inception",
-                        "Go into the dreams of a bussinessman to change his mind about something",
-                        "https://images-na.ssl-images-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SY1000_CR0,0,675,1000_AL_.jpg",
-                        "https://www.youtube.com/watch?v=66TuSJo4dZM")
+#Video
+inception = media.Video(        "Inception",
+                                "Go into the dreams of a bussinessman to change his mind about something",
+                                "https://images-na.ssl-images-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SY1000_CR0,0,675,1000_AL_.jpg",
+                                "https://www.youtube.com/watch?v=66TuSJo4dZM")
 
-interstellar = media.Movie("Intersellar",
-                           "Travel through the black hole to save humanity",
-                           "https://images-na.ssl-images-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SY1000_SX675_AL_.jpg",
-                           "https://www.youtube.com/watch?v=0vxOhd4qlnA")
+interstellar = media.Video(     "Intersellar",
+                                "Travel through the black hole to save humanity",
+                                "https://images-na.ssl-images-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SY1000_SX675_AL_.jpg",
+                                "https://www.youtube.com/watch?v=0vxOhd4qlnA")
 
-burnAfterReading = media.Movie("Burn After Reading",
-                               "Not so smart people causing havock by blackmail",
-                               "https://images-na.ssl-images-amazon.com/images/M/MV5BMTczNjQxODE0N15BMl5BanBnXkFtZTcwMzIxMjc3MQ@@._V1_SY1000_CR0,0,665,1000_AL_.jpg",
-                               "https://www.youtube.com/watch?v=SVCHSiRWjJM")
-fargo = media.Movie("Fargo",
-                    "A murder in  a small town caused unprecedented brutality in a fun way",
-                    "https://images-na.ssl-images-amazon.com/images/M/MV5BMTRmNGFlZGUtODY5ZC00YjJlLTk0ZDMtYjBhZDI1OGQ5YjU1XkEyXkFqcGdeQXVyMjk3NTUyOTc@._V1_.jpg",
-                    "https://www.youtube.com/watch?v=u4Je2WxsqWA")
-batmanVSSuperman = media.Movie("Batman V.S. Superman",
-                               "Rich human fights good looking alien who is imortal to everything but dies in the end",
-                               "https://images-na.ssl-images-amazon.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SY1000_CR0,0,675,1000_AL_.jpg",
-                               "https://www.youtube.com/watch?v=X7SiuQxhAjg")
-SuckerPunch = media.Movie ("Suckerpunch",
-                           "Explores disociation as a stress coping mechanism with allot of visual effects",
-                           "https://images-na.ssl-images-amazon.com/images/M/MV5BNDEwNGRlNjQtZjI4OC00ZmMxLWEyYmQtNGI1NDk4YmUyYTNkXkEyXkFqcGdeQXVyNjQ2MjQ5NzM@._V1_SY1000_CR0,0,676,1000_AL_.jpg",
-                           "https://www.youtube.com/watch?v=9k10AzCcMOM")
+burnAfterReading = media.Video( "Burn After Reading",
+                                "Not so smart people causing havock by blackmail",
+                                "https://images-na.ssl-images-amazon.com/images/M/MV5BMTczNjQxODE0N15BMl5BanBnXkFtZTcwMzIxMjc3MQ@@._V1_SY1000_CR0,0,665,1000_AL_.jpg",
+                                "https://www.youtube.com/watch?v=SVCHSiRWjJM")
 
-movies = [inception, interstellar, burnAfterReading, fargo, batmanVSSuperman, SuckerPunch]
+fargo = media.Video(            "Fargo",
+                                "A murder in  a small town caused unprecedented brutality in a fun way",
+                                "https://images-na.ssl-images-amazon.com/images/M/MV5BMTRmNGFlZGUtODY5ZC00YjJlLTk0ZDMtYjBhZDI1OGQ5YjU1XkEyXkFqcGdeQXVyMjk3NTUyOTc@._V1_.jpg",
+                                "https://www.youtube.com/watch?v=u4Je2WxsqWA")
+
+batmanVSSuperman = media.Video( "Batman V.S. Superman",
+                                "Rich human fights good looking alien who is imortal to everything but dies in the end",
+                                "https://images-na.ssl-images-amazon.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SY1000_CR0,0,675,1000_AL_.jpg",
+                                "https://www.youtube.com/watch?v=X7SiuQxhAjg")
+
+SuckerPunch = media.Video (     "Suckerpunch",
+                                "Explores disociation as a stress coping mechanism with allot of visual effects",
+                                "https://images-na.ssl-images-amazon.com/images/M/MV5BNDEwNGRlNjQtZjI4OC00ZmMxLWEyYmQtNGI1NDk4YmUyYTNkXkEyXkFqcGdeQXVyNjQ2MjQ5NzM@._V1_SY1000_CR0,0,676,1000_AL_.jpg",
+                                "https://www.youtube.com/watch?v=9k10AzCcMOM")
+#Music
+ParovStelar = media.Music(      "Company of Heroes 2",
+                                "Real time strategy game which strikes an excelent balance betweem tactics and strategy",
+                                "https://images-na.ssl-images-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SY1000_SX675_AL_.jpg",
+                                "null")
+
+#Games
+companyOfHeroes2 = media.Game(  "Company of Heroes 2",
+                                "Real time strategy game which strikes an excelent balance betweem tactics and strategy",
+                                "https://images-na.ssl-images-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SY1000_SX675_AL_.jpg",
+                                "null")
+
+movies = [inception, interstellar, burnAfterReading, fargo, batmanVSSuperman, SuckerPunch, ParovStelar, companyOfHeroes2]
 fresh_tomatoes.open_movies_page(movies)
-
-
-
--- media.py
+++ media.py
@@ -1,11 +1,32 @@
 import webbrowser
 
-class Movie():
-    def __init__(self, movie_title, movie_storyline, movie_image, movie_video):
-        self.title = movie_title
-        self.storyline = movie_storyline
-        self.poster_image_url = movie_image
+class Media():
+    def __init__(self, title, shortDescription, poster_image_url):
+        self.title = title
+        self.shortDescription = shortDescription
+        self.poster_image_url = poster_image_url
+
+class Video(Media):
+    def __init__(self, title, shortDescription, image, movie_video):
+        Media.__init__(self, title, shortDescription, image)
         self.trailer_youtube_url = movie_video
 
     def playVideo(self):
         webbrowser.open(self.video)
+
+class Music(Media):
+    def __init__(self, title, shortDescription, image, movie_video):
+        Media.__init__(self, title, shortDescription, image)
+        self.trailer_youtube_url = movie_video
+
+    def playVideo(self):
+        webbrowser.open(self.video)
+    
+class Game(Media):
+    def __init__(self, title, shortDescription, image, movie_video):
+        Media.__init__(self, title, shortDescription, image)
+        self.trailer_youtube_url = movie_video
+
+    def playVideo(self):
+        webbrowser.open(self.video)
+
